80e1b09035320dae01b1f9b30fce195ccb0a8da2
nicola gibson <nicola.k.gibson@gmail.com>
2018-11-24 15:07:12 +0000

    adding yargs

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let request = require("request");
+const argv = require("yargs").argv;
 
 let apiKey = "7544db96b0f129d2f80d356fb7c5de00";
-let city = "London";
+let city = argv.c || "London";
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 request(url, function(err, response, body) {
   if (err) {
     console.log("error:", error);
   } else {
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
     console.log("body:", body);
   }
 });
